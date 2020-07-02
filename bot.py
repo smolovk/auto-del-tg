374a505e145554ec177437d27a54009745073e79
smolovk <smolov326@gmail.com>
2020-07-02 17:10:58 +0300

    initial

--- bot.py
+++ bot.py
@@ -1,4 +1,28 @@
 import config
+import telebot
+import string
+import time
 
-print(config.TOKEN)
+bot = telebot.TeleBot(config.TOKEN)
+GROUP_ID = bot.get_chat("@dptl_reborn").id
+restrictedWords = ["test"]
 
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+
+    if message.text.startswith("/"):
+        bot.delete_message(message.chat.id, message.message_id)
+
+    swbucks = message.text.startswith("$")
+    
+    while True:
+        if (swbucks == False):
+            bot.delete_message(message.chat.id, message.message_id)
+            print(message.from_user.username)
+        time.sleep(300)
+
+
+
+
+bot.polling(none_stop=True, interval=0)
